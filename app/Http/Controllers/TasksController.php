352175bf4c3918b7449509585807906191a9bba5
tomoko.morita <morita.techacademy@gmail.com>
2021-04-07 09:57:27 +0000

    task content

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -37,7 +37,7 @@ class TasksController extends Controller
         // バリデーション
         $request->validate([
             'status' => 'required|max:10',   // 追加
-            'task' => 'required|max:255',
+            'content' => 'required|max:255',
         ]);
 
         // メッセージを作成
@@ -82,7 +82,7 @@ class TasksController extends Controller
         // バリデーション
         $request->validate([
             'status' => 'required|max:10',   // 追加
-            'task' => 'required|max:255',
+            'content' => 'required|max:255',
         ]);
 
         // idの値でメッセージを検索して取得
